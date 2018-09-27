e666694a656e07046e359ee157e2ed8939b54624
BunditSpace <spidar21@gmail.com>
2018-09-27 11:20:46 +0700

    init shop-dee

---
+++ src/store/modules/productStore.js
@@ -0,0 +1,30 @@
+import productService from '@/api/productService'
+import productCategoryService from '@/api/productCategoryService'
+
+const type = {
+
+}
+
+const state = {
+    
+}
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
